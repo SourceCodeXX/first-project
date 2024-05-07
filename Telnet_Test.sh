a7d58ff037f5e4e95ca1b66b031905238a5d1d96
SourceCodeXX <160187341+SourceCodeXX@users.noreply.github.com>
2024-05-06 20:38:42 -0700

    Update Telnet_Test.sh

--- Telnet_Test.sh
+++ Telnet_Test.sh
@@ -4,12 +4,6 @@
 read -p "Enter hostname or IP address to telnet: " host
 read -p "Enter port to telnet: " port
 
-# Check if telnet is installed
-if ! command -v telnet &> /dev/null; then
-    echo "Telnet is not installed. Please install telnet and try again."
-    exit 1
-fi
-
 # Try to telnet to the specified host and port
 if echo "quit" | telnet $host $port | grep "Escape character"; then
     echo "Telnet successful! Connection to $host on port $port is open."
